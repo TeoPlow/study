df32ab63177096869aefcb4d9660557f0147032f
TeoPlow <Egor294710@ya.ru>
2023-10-10 22:40:45 +0300

    add os_lab_1 and 2 and prolog 1

--- OOP/lab_02/four.h
+++ OOP/lab_02/four.h
@@ -4,32 +4,14 @@
 #include <string>
 #include <iostream>
 
-class Four {
-    std::vector<unsigned char> array;
+class Four
+{
 public:
-    Four();
-    Four(const size_t &, unsigned char);
-    Four(const std::initializer_list<unsigned char> &);
-    Four(const std::string &);
-    Four(const Four&);
-    Four(Four&&) noexcept;
-    virtual ~Four() noexcept;
-
-    size_t size() const noexcept;
-    std::string to_string() const noexcept;
-    int64_t to_decimal() const noexcept;
-
-    Four operator+(const Four &);
-    Four operator-(const Four &);
-    Four operator=(const Four &);
-    Four operator=(const std::string &);
-    bool operator<(const Four &);
-    bool operator>(const Four &);
-    bool operator==(const Four &);
-    bool operator!=(const Four &);
-    bool operator<=(const Four &);
-    bool operator>=(const Four &);
-
-    friend std::ostream& operator<<(std::ostream&, const Four&);
-    friend std::istream& operator>>(std::istream&, Four&);
+ Four();
+ Four(const size_t & n, unsigned char t = 0);
+ Four(const std::initializer_list< unsigned char> &t);
+ Four(const string &t);
+ Four(const Four& other);
+ Four(Four&& other) noexcept;
+ virtual ~Four() noexcept;
 };
