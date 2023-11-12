e58cb3264621e1cd9cc2db6982230fbf442e3599
TeoPlow <egorvotnomam294710@gmail.com>
2023-11-12 19:45:39 +0300

    lab_03 complete (99%)

--- OOP/lab_03/main.cpp
+++ OOP/lab_03/main.cpp
@@ -1,202 +1,110 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <iostream>
 #include <cmath>
 
-void add_point(int &x, int &y);
+#include "array.h"
+#include "figure.h"
+#include "trapeze.h"
+#include "square.h"
+#include "rectangle.h"
 
-class Figure {
-protected:
-    std::string name;
-public:
-    virtual void getInfo() = 0;
-};
-
-class Square : public Figure{
-
-public:
-    int x1, x2, x3, x4;
-    int y1, y2, y3, y4;
-
-    friend inline std::istream& operator>> (std::istream& in, Square &square) {
-        return in >> square.x1 >> square.y1 >> square.x2 >> square.y2 >> square.x3 >> square.y3 >> square.x4 >> square.y4;
-    }
-
-    Square() {
-        x1 = x2 = x3 = x4 = y1 = y2 = y3 = y4 = 0;
-    }
-
-    // Square(std::string name) {
-    //     this->name = name;
-    //     std::cout << "Введите координаты точек через Enter, например: 5 2" << std::endl;
-    //     add_point(x1, y1);
-    //     add_point(x2, y2);
-    //     add_point(x3, y3);
-    //     add_point(x4, y4);
-    // }
-
-    void getInfo() override{
-        float S = abs(sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2)) * sqrt(pow((x3 - x1), 2) + pow((y3 - y1), 2)));
-        float x_center = float(x1 + x2 + x3 + x4) / 4;
-        float y_center = float(y1 + y2 + y3 + y4) / 4;
-        std::cout << "Квадрат: "<< name << ". Площадь: " << S << ". Центр фигуры: " << x_center << " " << y_center << std::endl;
-        std::cout << "Точки: "<< x1 << " " << y1 << "  " << x2 << " " << y2 << "  " << x3 << " " << y3 << "  " << x4 << " " << y4 << std::endl;
-    }
-};
-
-class Rectangle : public Figure{
-
-public:
-    int x1, x2, x3, x4;
-    int y1, y2, y3, y4;
-
-    friend inline std::istream& operator>> (std::istream& in, Rectangle &rectangle) {
-        return in >> rectangle.x1 >> rectangle.y1 >> rectangle.x2 >> rectangle.y2 >> rectangle.x3 >> rectangle.y3 >> rectangle.x4 >> rectangle.y4;
-    }
-
-    // Rectangle(std::string name) {
-    //     this->name = name;
-    //     // std::cout << "Введите координаты точек через Enter, например: 5 2" << std::endl;
-    //     // add_point(x1, y1);
-    //     // add_point(x2, y2);
-    //     // add_point(x3, y3);
-    //     // add_point(x4, y4);
-    // }
-
-    Rectangle() {
-        x1 = x2 = x3 = x4 = y1 = y2 = y3 = y4 = 0;
-    }
-    float area() {
-        float S = abs(sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2)) * sqrt(pow((x3 - x1), 2) + pow((y3 - y1), 2)));
-        return S;
-    }
-
-    std::pair<float, float> Huinia () {
-        float x_center = float(x1 + x2 + x3 + x4) / 4;
-        float y_center = float(y1 + y2 + y3 + y4) / 4;
-        return {x_center, y_center};
-    }
-
-    void getInfo() override{
-        std::cout << "Прямоугольник: "<< name << ". Площадь: " << S << ". Центр фигуры: " << x_center << " " << y_center << std::endl;
-        std::cout << "Точки: "<< x1 << " " << y1 << "  " << x2 << " " << y2 << "  " << x3 << " " << y3 << "  " << x4 << " " << y4 << std::endl;
-    }
-};
-
-class Trapeze : public Figure{
-
-public:
-    int x1, x2, x3, x4;
-    int y1, y2, y3, y4;
-
-    friend inline std::istream& operator>> (std::istream& in, Trapeze &trapeze) {
-        return in >> trapeze.x1 >> trapeze.y1 >> trapeze.x2 >> trapeze.y2 >> trapeze.x3 >> trapeze.y3 >> trapeze.x4 >> trapeze.y4;
-    }
-
-    Trapeze() {
-        x1 = x2 = x3 = x4 = y1 = y2 = y3 = y4 = 0;
-    }
-
-    // Trapeze(std::string name) {
-    //     this->name = name;
-    //     std::cout << "Введите координаты точек через Enter, например: 5 2" << std::endl;
-    //     add_point(x1, y1);
-    //     add_point(x2, y2); 
-    //     add_point(x3, y3);
-    //     add_point(x4, y4);
-    // }
-
-    void getInfo() override{
-        float S = abs(0.5 * (x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2)));
-        float x_center = float(x1 + x2 + x3 + x4) / 4;
-        float y_center = float(y1 + y2 + y3 + y4) / 4;
-        std::cout << "Трапеция: "<< name << ". Площадь: " << S << ". Центр фигуры: " << x_center << " " << y_center << std::endl;
-        std::cout << "Точки: "<< x1 << " " << y1 << "  " << x2 << " " << y2 << "  " << x3 << " " << y3 << "  " << x4 << " " << y4 << std::endl;
-    }
-};
-
-class Array {
-private:
-    Figure ** arr;
-    size_t size_arr;
-    Array () {
-        arr = new Figure * [0];
-    }
-
-    void increase_arr () {
-        Figure ** arr2 = new Figure * [size_arr + 1];
-        for (int i = 0; i < size_arr; i++) {
-            arr2[i] = arr[i];
-        }
-        delete[] arr;
-        arr = arr2;
-        ++size_arr;
-    }
-
-public:
-    void add_figure_to_arr(Figure * figure_name) {
-        increase_arr();
-        arr[size_arr - 1] = figure_name;
-    }
-
-    Figure * operator[] (size_t i) {
-        return arr[i];
-    }
-};
-
-void add_figure() {
-    std::string name;
+int main() {
     bool flag;
-
-    do {
-        flag = 1;
-        std::cout << "Введите название фигуры: " << std::endl;
-        std::cin >> name;
-
-        if (name.find("square") == 0) {
-            Square square;
-            std::cin >> square;
-            // square.getInfo();
-        } else if (name.find("rectangle") == 0) {
-            Rectangle rectang;
-            std::cin >> rectang;
-            rectang.getInfo();
-        } else if (name.find("trapeze") == 0) {
-            Trapeze trap;
-            std::cin >> trap;
-            trap.getInfo();
-        } else {
-            std::cout << "Неправильное название, повторите попытку" << std::endl;
-            flag = 0;
-        }
-    } while (flag == 0);
-}
-
-// void add_point(int &x, int &y) {
-//     int* point = new int[2];
-//     int index = -1;
-    
-//     for ( int i = 0; i < 2; i++ ) {
-//         std::cin >> point[i];
-//         if ( point[i] < 0 )
-//             index = i;
-//     }
-//     x = point[0];
-//     y = point[1];
-        
-//     delete[] point;
-// }
-
-int main () {
-// Input figure
+    std::string name;
+    int fig1, fig2;
     char answer;
+    int i = 0;
+    Array q = Array(3);
+    std::cout << "(Вводите точки так: 0 0, 1 0, 0 1, 1 1)" << std::endl;
     std::cout << "Существующие фигуры: square, rectangle, trapeze" << std::endl;
 
     do {
-    add_figure();
-    std::cout << "Хотите добавить ещё одну фигуру? (y/n)" << std::endl;
-    std::cin >> answer;
+        do {
+            flag = 1;
+            std::cout << "Введите название фигуры: " << std::endl;
+            std::cin >> name;
+            if (name.find("square") == 0) {
+                std::cout << "Введите 4 точки X и Y" << std::endl;
+                Square* sq = new Square;
+                std::cin >> *sq;
+                q.add_figure(i, sq);
+                
+            } else if (name.find("rectangle") == 0) {
+                Rectangle rectang;
+                std::cout << "Введите 4 точки X и Y" << std::endl;
+                Rectangle* rec = new Rectangle;
+                std::cin >> *rec;
+                q.add_figure(i, rec);
+
+            } else if (name.find("trapeze") == 0) {
+                Trapeze trap;
+                std::cout << "Введите 4 точки X и Y" << std::endl;
+                Trapeze* tr = new Trapeze;
+                std::cin >> *tr;
+                q.add_figure(i, tr);
+
+            } else {
+                std::cout << "Неправильное название, повторите попытку" << std::endl;
+                flag = 0;
+            }
+        } while (flag == 0);
+        std::cout << "Хотите добавить ещё одну фигуру? (y/n)" << std::endl;
+        std::cin >> answer;
+        i += 1;
     } while (answer == 'y' || answer == 'Y');
 
+
+    for(int j = 0; j < i; j++) {
+        const Figure* figur = q[j];
+        std::cout << "Фигура: " << j + 1 << "\n";
+        std::cout << "Площадь: " << figur->area() << std::endl;
+        std::cout << "Центр: " << figur->center().first << ' ' << figur->center().second << std::endl;
+        std::cout << std::endl;
+    }
+
+    do {
+        std::cout << std::endl;
+        std::cout << "Что желаете сделать с фигурами?" << std::endl;
+        std::cout << "Функции: delete, total_area, getInfo, end" << std::endl;
+        std::cin >> name;
+
+        if (name.find("delete") == 0) {
+            std::cout << "Какую фигуру удалить?" << std::endl;
+            std::cin >> fig1;
+            if (q[fig1 - 1] == nullptr) {
+                std::cout << "Фигуры и так небыло в массиве" << std::endl;
+            } else {
+                q.del_figure(fig1 - 1);
+                i -= 1;
+                std::cout << "Фигура удалена" << std::endl;
+            }
+        }
+        else if (name.find("total_area") == 0) {
+            double avg = 0;
+            for (int j = 0; j < i; j++) {
+                const Figure* figur = q[j];
+                avg += figur->area();
+            }
+            avg /= i;
+            std::cout << "Средняя площадь: " << avg << std::endl;
+        }
+        else if (name.find("getInfo") == 0) {
+            std::cout << "Номер фигуры?" << std::endl;
+            std::cin >> fig1;
+            const Figure* figur = q[fig1 - 1];
+            if (q[fig1 - 1] == nullptr) {
+                std::cout << "Фигуры нет в массиве..." << std::endl;
+            } else {
+                std::cout << "Площадь: " << figur->area() << std::endl;
+                std::cout << "Центр: " << figur->center().first << ' ' << figur->center().second << std::endl;
+            }
+        }
+        else if ((name.find("end") == 0)) {
+            return 0;
+        }
+        else {
+            std::cout << "Неправильная функция, повторите попытку" << std::endl;
+            flag = 0;
+        }
+    } while ((name.find("end") != 0));
     return 0;
-} 
+}
---
+++ OOP/lab_03/rectangle.cpp
@@ -0,0 +1,67 @@
+#include <exception>
+#include <cmath>
+
+#include "rectangle.h"
+
+Rectangle::Rectangle(){
+    x = {0.0,0.0,0.0,0.0};
+    y = {0.0,0.0,0.0,0.0};
+    x_center = 0.0;
+    y_center = 0.0;
+    surface = 0;
+}
+
+Rectangle::Rectangle(std::vector<double> &x,std::vector<double> &y) {
+    this->x = x;
+    this->y = y;
+}
+
+Rectangle::~Rectangle(){
+    x = {};
+    y = {};
+    x_center = 0.0;
+    y_center = 0.0;
+    surface = 0;
+}
+
+bool Rectangle::operator==(Rectangle &other) const {
+    if(surface == other.surface){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+Rectangle &Rectangle::operator=(Rectangle &other) {
+    x = other.x;
+    y = other.y;
+    x_center=other.x_center;
+    y_center=other.y_center;
+    surface=other.surface;
+    return *this;
+}
+Rectangle &Rectangle::operator=(Rectangle &&other) {
+    x = std::move(other.x);
+    y = std::move(other.y);
+    x_center = std::move(other.x_center);
+    y_center = std::move(other.y_center);
+    surface = std::move(other.surface);
+    return *this;
+}
+
+std::pair<double,double> Rectangle::center() const {
+    double x_center = double(x[0] + x[1] + x[2] + x[3]) / 4;
+    double y_center = double(y[0] + y[1] + y[2] + y[3]) / 4;
+
+    return {x_center, y_center};
+}
+
+double Rectangle::area() const {
+    double surface = abs(sqrt(pow((x[1] - x[0]), 2)
+    + pow((y[1] - y[0]), 2)) 
+    * sqrt(pow((x[2] - x[0]), 2) 
+    + pow((y[2] - y[0]), 2)));
+
+    return surface;
+}
