3d0eb66873e4c92413a191ec60892e7375f93de7
TeoPlow <egorvotnomam294710@gmail.com>
2023-11-12 10:56:17 +0300

    помогите.......

---
+++ OOP/kek/trapeze.cpp
@@ -0,0 +1,65 @@
+#include "trapeze.h"
+#include <exception>
+#include <cmath>
+
+Trapeze::Trapeze() {
+    x = {0.0,0.0,0.0,0.0};
+    y = {0.0,0.0,0.0,0.0};
+    x_center = 0.0;
+    y_center = 0.0;
+    surface = 0;
+}
+
+Trapeze::Trapeze(std::vector<double> &x,std::vector<double> &y) {
+    this->x = x;
+    this->y = y;
+}
+
+Trapeze::~Trapeze() {
+    x = {};
+    y = {};
+    x_center = 0.0;
+    y_center = 0.0;
+    surface = 0;
+}
+
+bool Trapeze::operator==(Trapeze &other) const {
+    if(surface == other.surface){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+Trapeze &Trapeze::operator=(Trapeze &other){
+    x = other.x;
+    y = other.y;
+    x_center = other.x_center;
+    y_center = other.y_center;
+    surface = other.surface;
+    return *this;
+}
+Trapeze &Trapeze::operator=(Trapeze &&other){
+    x = std::move(other.x);
+    y = std::move(other.y);
+    x_center = std::move(other.x_center);
+    y_center = std::move(other.y_center);
+    surface = std::move(other.surface);
+    return *this;
+}
+
+std::pair<double,double> Trapeze::center() const {
+    double x_center = double(x[0] + x[1] + x[2] + x[3]) / 4;
+    double y_center = double(y[0] + y[1] + y[2] + y[3]) / 4;
+
+    return {x_center, y_center};
+}
+
+double Trapeze::area() const {
+    double surface = abs(0.5 * (x[0] * 
+    (y[1] - y[2]) + x[1] * (y[2] - y[0]) + 
+    x[2] * (y[0] - y[1])));
+
+    return surface;
+}
---
+++ OOP/kek/trapeze.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <iostream>
+#include <utility>
+#include <vector>
+
+#include "figure.h"
+
+class Trapeze : public Figure{
+protected:
+    std::vector<double> x;
+    std::vector<double> y;
+    double surface;
+    double x_center;
+    double y_center;
+
+public:
+    Trapeze();
+    Trapeze(std::vector<double> &x,std::vector<double> &y);
+    ~Trapeze();
+    bool operator==(Trapeze &other) const;
+    Trapeze &operator=(Trapeze &other);
+    Trapeze &operator=(Trapeze &&other);
+    friend inline std::ostream& operator<<(std::ostream& out, const Trapeze& s);
+    friend inline std::istream& operator>>(std::istream& in, Trapeze & s);
+
+    double area() const override;
+
+    std::pair<double,double> center() const override;
+
+    operator double() const override{
+        return surface;
+    }
+};
+
+inline std::ostream &operator<<(std::ostream& out, const Trapeze& s){
+    return out << s.x[0] << ' ' << s.y[0] << "%n" 
+    << s.x[1] << ' ' << s.y[1] << "%n" 
+    << s.x[2] << ' ' << s.y[2] << "%n" 
+    << s.x[3] << ' ' << s.y[3] << "%n";
+}
+
+inline std::istream& operator>>(std::istream& in, Trapeze& s) {
+    return in >>
+    s.x[0] >> s.y[0] >>
+    s.x[1] >> s.y[1] >> 
+    s.x[2] >> s.y[2] >> 
+    s.x[3] >> s.y[3];
+}
