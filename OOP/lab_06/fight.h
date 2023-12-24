8f79ad01d05b6949e41cbeb81e6d7e7792f6e5a7
TeoPlow <144553415+TeoPlow@users.noreply.github.com>
2023-12-24 17:47:27 +0300

    Delete OOP/lab_06 directory

--- OOP/lab_06/fight.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "../include/factory.h"
-#include "Visitor.h"
-
-set_t fight(const set_t &array, size_t distance);
--- OOP/lab_06/npc_pair.cpp
+++
@@ -1,6 +0,0 @@
-#include "../include/npc_pair.h"
-
-int PairNPC::accept(Visitor& visitor)
-{
-    return visitor.visit(*this);
-}
