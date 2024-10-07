30bb5d5f32d4737f477bbcfe417e6bfc95a83dff
TeoPlow <Egor294710@yandex.ru>
2024-10-07 23:34:00 +0300

    lab5 done ...bad... >_<

--- Discrete_Analysis/lab5/main.cpp
+++ Discrete_Analysis/lab5/main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <fstream>
 #include <algorithm>
+#include <unordered_map>
 
 struct Symbol {
     char symb;
@@ -10,61 +11,151 @@ struct Symbol {
     int eq[2];
 };
 
-bool sort_key(const Symbol &a, const Symbol &b) {
-    return (a.eq[0] == b.eq[0]) ? (a.eq[1] < b.eq[1]) : (a.eq[0] < b.eq[0]);
+void printVector(const std::vector<int>& vec) {
+    for (int num : vec) {
+        std::cout << num;
+    }
+    std::cout << std::endl;
 }
 
-std::vector<Symbol> suffixArray(const std::string &txt) {
-    int n = txt.size();
-    std::vector<Symbol> suff(n);
+void symbCountingSort(std::vector<Symbol>& arr, char max_symb) {
+    max_symb = max_symb;
+    int size = arr.size();
+    std::vector<int> count(max_symb + 1, 0);
+    std::vector<Symbol> output(size);
 
-    for (int i = 0; i < n; i++) {
-        suff[i].ind = i;
-        suff[i].eq[0] = txt[i] - 'a';
-        suff[i].eq[1] = (i + 1 < n) ? (txt[i + 1] - 'a') : -1;
+    for (int i = 0; i < size; i++) {
+        count[arr[i].symb]++;
     }
 
-    sort(suff.begin(), suff.end(), sort_key);
+    for (int i = 1; i <= max_symb; i++) {
+        count[i] += count[i - 1];
+    }
 
-    std::vector<int> ind(n);
+    for (int i = size - 1; i >= 0; i--) {
+        output[count[arr[i].symb] - 1] = arr[i];
+        count[arr[i].symb]--;
+    }
 
-    for (int k = 4; k < 2 * n; k *= 2) {
-        int eq = 0;
-        int prev_eq = suff[0].eq[0];
-        suff[0].eq[0] = eq;
-        ind[suff[0].ind] = 0;
+    for (int i = 0; i < size; i++) {
+        arr[i] = output[i];
+    }
+}
 
-        for (int i = 1; i < n; i++) {
-            if (suff[i].eq[0] == prev_eq && suff[i].eq[1] == suff[i - 1].eq[1]) {
-                prev_eq = suff[i].eq[0];
-                suff[i].eq[0] = eq;
-            } else {
-                prev_eq = suff[i].eq[0];
-                suff[i].eq[0] = ++eq;
+void eqCountingSort(std::vector<Symbol>& arr, int max_eq) {
+    int size = arr.size();
+    std::vector<int> count(max_eq + 1, 0);
+    std::vector<Symbol> output(size);
+
+    for (int i = 0; i < size; i++) {
+        count[arr[i].eq[0]]++;
+    }
+
+    for (int i = 1; i <= max_eq; i++) {
+        count[i] += count[i - 1];
+    }
+
+    for (int i = size - 1; i >= 0; i--) {
+        output[count[arr[i].eq[0]] - 1] = arr[i];
+        count[arr[i].eq[0]]--;
+    }
+
+    for (int i = 0; i < size; i++) {
+        arr[i] = output[i];
+    }
+}
+
+std::vector<int> suffixArray(const std::string &txt) {
+    int size = txt.size();
+    std::vector<Symbol> text_arr(size);
+
+    // Раставляем индексы
+    char max_symb = '$';
+    for (int i = 0; i < size; i++) {
+        if (i >= size) {
+            text_arr[i].symb = '$';
+            text_arr[i].ind = i;
+        } else {
+            if (txt[i] > max_symb) {
+                max_symb = txt[i];
             }
-            ind[suff[i].ind] = i;
+            text_arr[i].symb = txt[i];
+            text_arr[i].ind = i;
         }
-
-        for (int i = 0; i < n; i++) {
-            int next_index = suff[i].ind + k / 2;
-            suff[i].eq[1] = (next_index < n) ? suff[ind[next_index]].eq[0] : -1;
-        }
-
-        sort(suff.begin(), suff.end(), sort_key);
     }
 
-    std::vector<Symbol> suffix_array(n);
-    for (int i = 0; i < n; i++) {
-        suffix_array[i].ind = suff[i].ind;
+    // Сортируем подсчётом посимвольно
+    symbCountingSort(text_arr, max_symb);
+
+    // Добавляем эквиваленты
+    char old_symb = '$';
+    int eq = 0;
+    for (int i = 0; i < size; i++) {
+        if (text_arr[i].symb != old_symb) {
+            old_symb = text_arr[i].symb;
+            eq++;
+        }
+        text_arr[i].eq[0] = eq;
+        text_arr[i].eq[1] = 0;
     }
 
-    return suffix_array;
+    // Шараварим
+    for (int k = 1; k < size; k *= 2) {
+
+        // Добавялем в словарик индексы и их номер
+        std::unordered_map<int, int> map;
+        for (int i = 0; i < size; i++) {
+            map[text_arr[i].ind] = i;
+        }
+
+        // Добавляем эквиваленты
+        int old_eq = 0;
+        int old_eq2 = text_arr[0].eq[1];
+        eq = 0;
+        for (int i = 0; i < size; i++) {
+            if (text_arr[i].eq[0] != old_eq) {
+                old_eq = text_arr[i].eq[0];
+                eq++;
+            } else {
+                if (text_arr[i].eq[1] != old_eq2) {
+                    eq++;
+                }
+            }
+            old_eq2 = text_arr[i].eq[1];
+            text_arr[i].eq[0] = eq;
+            text_arr[i].eq[1] = 0;
+        }
+
+        // Отнимаем степень двойки у индексов по модулю
+        std::vector<Symbol> temp_text_arr(size);
+        for (int i = 0; i < size; i++) {
+            temp_text_arr[i] = text_arr[map[(text_arr[i].ind - k + size) % size]];
+            temp_text_arr[i].eq[1] = text_arr[i].eq[0];
+        }
+        for (int i = 0; i < size; i++) {
+            text_arr[i] = temp_text_arr[i];
+        }
+
+        // Сортируем подсчётом по эквивалентам
+        eqCountingSort(text_arr, eq);
+
+        if (eq == (size - 1)) {
+            break;
+        }
+    }
+
+    // Возвращаем суф.массив
+    std::vector<int> suff_arr(size);
+    for (int i = 0; i < size; i++) {
+        suff_arr[i] = text_arr[i].ind;
+    }
+    return suff_arr;
 }
 
-std::string searchSubstr(std::string &text, std::string &pattern, std::vector<Symbol> &suff_arr) {
+std::string searchSubstr(std::string &text, std::string &pattern, std::vector<int> &suff_arr) {
     std::string answer = "";
     std::vector<int> results;
-    int n = text.size();
+    int n = suff_arr.size();
     int m = pattern.size();
 
     if (pattern == "") {
@@ -74,7 +165,7 @@ std::string searchSubstr(std::string &text, std::string &pattern, std::vector<Sy
     int low = 0, high = n - 1;
     while (low < high) {
         int mid = (low + high) / 2;
-        int start = suff_arr[mid].ind;
+        int start = suff_arr[mid];
         std::string suffix = text.substr(start, std::min(n - start, m));
 
         if (suffix < pattern) {
@@ -84,7 +175,7 @@ std::string searchSubstr(std::string &text, std::string &pattern, std::vector<Sy
         }
     }
 
-    std::string suffix = text.substr(suff_arr[low].ind, std::min(n - suff_arr[low].ind, m));
+    std::string suffix = text.substr(suff_arr[low], std::min(n - suff_arr[low], m));
     if (suffix != pattern) {
         return answer;
     }
@@ -93,7 +184,7 @@ std::string searchSubstr(std::string &text, std::string &pattern, std::vector<Sy
     high = n - 1;
     while (low < high) {
         int mid = (low + high + 1) / 2;
-        int start = suff_arr[mid].ind;
+        int start = suff_arr[mid];
         std::string suffix = text.substr(start, std::min(n - start, m));
 
         if (suffix > pattern) {
@@ -104,7 +195,7 @@ std::string searchSubstr(std::string &text, std::string &pattern, std::vector<Sy
     }
 
     for (int i = low; i >= lower; i--) {
-        results.push_back(suff_arr[i].ind);
+        results.push_back(suff_arr[i]);
     }
 
     sort(results.begin(), results.end());
@@ -119,7 +210,8 @@ std::string searchSubstr(std::string &text, std::string &pattern, std::vector<Sy
 }
 
 int main() {
-    std::ifstream file("tests/1000.txt");
+
+    std::ifstream file("tests/wrong.txt");
 
     if (!file.is_open()) {
         std::cerr << "Ошибка: Не удалось открыть файл!\n";
@@ -129,19 +221,32 @@ int main() {
     int num = 1;
     std::string string;
     std::string text;
-    std::vector<Symbol> vec;
+    std::vector<int> suff_arr;
     std::string answer;
 
     bool flag = false;
     while (std::getline(file, string)) {
         if (flag == false) {
+            int size = 1;
+            int size2 = string.size();
+            if (size2 > 0 && (size2 & (size2 - 1)) == 0) {
+                string += '$';
+            }
+            while (size < size2) {
+                size *= 2;
+            }
+            for (int i = 0; i < (size - size2); i++) {
+                string += '$';
+            }
+            
             text = string;
-            vec = suffixArray(string);
+            suff_arr = suffixArray(string);
+            printVector(suff_arr);
             flag = true;
         } else {
-            answer = searchSubstr(text, string, vec);
+            answer = searchSubstr(text, string, suff_arr);
             if (answer != "") {
-                std::cout << num << ": " << searchSubstr(text, string, vec) << std::endl;
+                std::cout << num << ": " << answer << std::endl;
             }
         	num += 1;
         }
